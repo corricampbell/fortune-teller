22513ef203df119c9644d17f4b252594027915bb
corricampbell <campbell.corri@gmail.com>
2019-01-06 16:48:01 -0500

    Updated Fortune-Teller

--- src/FortuneTellerApp.java
+++ src/FortuneTellerApp.java
@@ -4,6 +4,8 @@ public class FortuneTellerApp {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		String yearsRetirement = "0";
+		String vacationHome = "0";
 
 		System.out.println("FORTUNE TELLER");
 //Part 1
@@ -25,33 +27,25 @@ public class FortuneTellerApp {
 		int numberSibling = input.nextInt();
 
 //Part 2
-		System.out.println("Your years to retirement: ");
-		   if (numberAge % 2 == 0) {
-			System.out.println("You will retire in 10 years.");
-		  } else {
-			System.out.println("You will retire in 15 years.");
-
-		System.out.println("Your vacation home location:");
-			if (numberSibling == 0) {
-				System.out.println("Detroit");
-			} else {
-				System.out.println("Hawaii");
-			}
-		System.out.println("Your mode of transportation:");
-		if (favoriteColor == green) {
-			System.out.println("Jaguar");
-			else {
-				System.out.println("Pinto");
-			}
+		System.out.println("Years until retirement:");
+		if (numberAge % 2 == 0) {
+			yearsRetirement = "10";
+		} else {
+			yearsRetirement = "15";
 		}
-		}System.out.println("Your bank balance at retirement:");
-			if (birthMonth > 12)
-				System.out.println("$0");
-			else 
-				System.out.println("1 million dollars");			
-//Part 3
-			
-		System.out.println(firstName + lastName "will retire in" + numberAge "with" birthMonth "in the bank, a vacation home in" + numberSibling ", and travel by" + favoriteColor "." );
-	}
 
-}
+		System.out.println("Vacation Home Location:");
+		if (numberSibling <= 0) {System.out.println("Detroit");} 
+		else if (numberSibling == 1) {System.out.println("Hawaii");}
+		else if (numberSibling == 2) {System.out.println("Florida");}
+		else if (numberSibling == 3) {System.out.println("Idaho");}
+		else if (numberSibling > 3) {System.out.println("Texas");}
+		}
+		
+		
+			System.out.println(yearsRetirement);
+
+		System.out.println(firstName);
+
+	}
+}}
